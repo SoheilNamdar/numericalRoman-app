491705c785a1b9de68c22ab0e5d4e9699e52295e
SoheilNamdar <soheilnamdar@yahoo.com>
2019-06-10 18:05:37 +0200

    clean code

--- src/main/java/model/NumericalRomain.java
+++ src/main/java/model/NumericalRomain.java
@@ -1,12 +1,10 @@
 package model;
 
-import java.util.ArrayList;
-
 public class NumericalRomain {
     private String romanNumber = "";
 
     public String convert(int arabicNumber){
-        for(int i=0 ; i<arabicNumber ; i++) {
+        for (int i = 0 ; i < arabicNumber ; i++) {
             romanNumber += "I";
         }
         return romanNumber;
