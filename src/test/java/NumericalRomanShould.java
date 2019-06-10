c7b7ffcdbdf2446cc51f26132402cb97103b06fa
SoheilNamdar <soheilnamdar@yahoo.com>
2019-06-10 18:03:05 +0200

    refactor tests

--- src/test/java/NumericalRomanShould.java
+++ src/test/java/NumericalRomanShould.java
@@ -1,20 +1,29 @@
 import model.NumericalRomain;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumericalRomanShould {
+
+    private NumericalRomain numericalRomain;
+
+    @Before
+    public void setUp() {
+        numericalRomain = new NumericalRomain();
+    }
+
     @Test
     public void convert_1_to_I() {
-        Assert.assertEquals("I", new NumericalRomain().convert(1));
+        Assert.assertEquals("I", numericalRomain.convert(1));
     }
 
     @Test
     public void convert_2_to_II() {
-        Assert.assertEquals("II", new NumericalRomain().convert(2));
+        Assert.assertEquals("II", numericalRomain.convert(2));
     }
 
     @Test
     public void convert_3_to_III() {
-        Assert.assertEquals("III", new NumericalRomain().convert(3));
+        Assert.assertEquals("III", numericalRomain.convert(3));
     }
 }
