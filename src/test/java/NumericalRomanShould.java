5673ec8a5b03b1601633a128315cfc42f0863fb6
SoheilNamdar <soheilnamdar@yahoo.com>
2019-06-10 18:00:04 +0200

    case 1 2 3

--- src/test/java/NumericalRomanShould.java
+++ src/test/java/NumericalRomanShould.java
@@ -1,8 +1,20 @@
+import model.NumericalRomain;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NumericalRomanShould {
     @Test
-    public void nothing() {
+    public void convert_1_to_I() {
+        Assert.assertEquals("I", new NumericalRomain().convert(1));
+    }
 
+    @Test
+    public void convert_2_to_II() {
+        Assert.assertEquals("II", new NumericalRomain().convert(2));
+    }
+
+    @Test
+    public void convert_3_to_III() {
+        Assert.assertEquals("III", new NumericalRomain().convert(3));
     }
 }
