883640ee99b94de730f72b7cf94ca4ecf81800db
SoheilNamdar <soheilnamdar@yahoo.com>
2019-06-15 19:55:38 +0200

    case 1.2 and 3

--- src/main/java/model/NumericalRomain.java
+++ src/main/java/model/NumericalRomain.java
@@ -1,21 +1,13 @@
 package model;
 
 public class NumericalRomain {
-    private String romanNumber = "";
 
-    public String convert(int arabicNumber){
-        for (int i = 0 ; i < arabicNumber ; i++) {
-            romanNumber += "I";
+    public String convert(int arabicNumber) {
+        String s = "";
+        for (int i=0 ; i<arabicNumber ; i++) {
+            s += "I";
         }
-        if (arabicNumber == 6) {
-            romanNumber = "VI";
-        }
-        if (arabicNumber == 7) {
-            romanNumber = "VII";
-        }
-        if (arabicNumber == 8) {
-            romanNumber = "VIII";
-        }
-        return romanNumber;
+        return s;
     }
 }
+
--- src/test/java/NumericalRomanShould.java
+++ src/test/java/NumericalRomanShould.java
@@ -1,5 +1,4 @@
 import model.NumericalRomain;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,18 +28,4 @@ public class NumericalRomanShould {
         assertEquals("III", numericalRomain.convert(3));
     }
 
-    @Test
-    public void convert_6_to_VI() {
-        assertEquals("VI", numericalRomain.convert(6));
-    }
-
-    @Test
-    public void convert_7_to_VII() {
-        assertEquals("VII", numericalRomain.convert(7));
-    }
-
-    @Test
-    public void convert_8_to_VIII() {
-        assertEquals("VIII", numericalRomain.convert(8));
-    }
 }
