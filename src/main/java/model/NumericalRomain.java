fbc26c8ff5660662ef074ff435be21ca12f40a9c
SoheilNamdar <soheilnamdar@yahoo.com>
2019-06-16 17:42:42 +0200

    transformation priority premise

--- src/main/java/model/NumericalRomain.java
+++ src/main/java/model/NumericalRomain.java
@@ -2,26 +2,44 @@ package model;
 
 public class NumericalRomain {
 
-    public String convert(int arabicNumber) {
-        String romanNumber = "";
+    public String romanFor(int decimal){
+        String roman = "";
 
-        if (arabicNumber > 10 ) {
-            romanNumber += "X";
-            arabicNumber -= 10;
+        /*for(RomanToDecimal romanToDecimal : RomanToDecimal.values()) {
+            while (decimal >= romanToDecimal.decimal) {
+                roman += romanToDecimal.roman ;
+                decimal -= romanToDecimal.decimal;
+            }
+        }*/
+        while (decimal>10) {
+            roman += "X";
+            decimal -= 10;
         }
 
-        if (arabicNumber > 5 ) {
-            romanNumber += "V";
-            arabicNumber -= 5;
+        if( decimal > 5) {
+            roman += "V";
+            decimal -= 5;
         }
 
-        for (int i = 0 ; i < arabicNumber ; i++) {
-            romanNumber += "I";
+        for(int i=0 ; i<decimal ; i++) {
+                roman += "I";
         }
-
-        return romanNumber;
+        return roman;
     }
 
+    enum RomanToDecimal {
+        Five(5, "V"),
+        Ten(10, "X");
+
+
+        public final int decimal;
+        public final String roman;
+
+        RomanToDecimal(int decimal, String roman) {
+            this.decimal = decimal;
+            this.roman = roman;
+        }
+    }
 
 }
 
--- src/test/java/NumericalRomanShould.java
+++ src/test/java/NumericalRomanShould.java
@@ -15,46 +15,58 @@ public class NumericalRomanShould {
 
     @Test
     public void convert_1_to_I() {
-        assertEquals("I", numericalRomain.convert(1));
+        assertEquals("I", numericalRomain.romanFor(1));
     }
 
     @Test
     public void convert_2_to_II() {
-        assertEquals("II", numericalRomain.convert(2));
+        assertEquals("II", numericalRomain.romanFor(2));
     }
 
     @Test
     public void convert_3_to_III() {
-        assertEquals("III", numericalRomain.convert(3));
+        assertEquals("III", numericalRomain.romanFor(3));
     }
 
     @Test
     public void convert_6_to_VI() {
-        assertEquals( "VI", numericalRomain.convert(6));
+        assertEquals( "VI", numericalRomain.romanFor(6));
     }
 
     @Test
     public void convert_7_to_VII() {
-        assertEquals("VII", numericalRomain.convert(7));
+        assertEquals("VII", numericalRomain.romanFor(7));
     }
 
     @Test
     public void convert_8_to_VIII() {
-        assertEquals("VIII", numericalRomain.convert(8));
+        assertEquals("VIII", numericalRomain.romanFor(8));
     }
 
     @Test
     public void convert_11_to_XI() {
-        assertEquals("XI", numericalRomain.convert(11));
+        assertEquals("XI", numericalRomain.romanFor(11));
     }
 
     @Test
     public void convert_12_to_XII() {
-        assertEquals("XII", numericalRomain.convert(12));
+        assertEquals("XII", numericalRomain.romanFor(12));
     }
 
     @Test
     public void convert_13_to_XIII() {
-        assertEquals("XIII", numericalRomain.convert(13));
+        assertEquals("XIII", numericalRomain.romanFor(13));
+    }
+
+    @Test
+    public void convert_17_to_XVII() { assertEquals("XVII", numericalRomain.romanFor(17));
+    }
+
+    @Test
+    public void convert_17_to_XXI() { assertEquals("XXI", numericalRomain.romanFor(21));
+    }
+
+    @Test
+    public void convert_37_to_XXXVII() { assertEquals("XXXVII", numericalRomain.romanFor(37));
     }
 }
